8b45ca662d46d81ad53e94ee378232721241cee9
Christopher Drumm <chris@cdrumm.co>
2019-05-03 12:19:26 -0700

    Update main.py

--- examples/main.py
+++ examples/main.py
@@ -3,7 +3,7 @@ import threading
 
 def main():
     connection = Textplus()
-    connection.send_message("+18312878759", "cool dudeeeee") #number must be like: 18312345423 (mind the country code)
+    connection.send_message("+1numberhere", "some message") #number must be like: 18312345423 (mind the country code)
     
     
 main()
